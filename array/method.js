a06f9e84e924df2bbf1a6f990cc7365bdc9ba0ed
mainuddin-sani <main12sani@gmail.com>
2023-06-12 23:29:00 +0600

    array slice

---
+++ array/method.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple"];
+// console.log(fruits.slice(2,4));
+
+// console.log(fruits.slice(-4,-1));
+// fruits.splice(1,0,'sani','mani');
+fruits.splice(1,3,'sani','mani');
+console.log(fruits);
